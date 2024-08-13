2b9932e4ac8b177e35c0783c3ed067f814e8a20f
Bill Mills <bill.mills@linaro.org>
2024-08-13 10:33:30 -0400

    build-host-setup: quiet the apt-get commands
    Be less verbose for the apt-get commands.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- scripts/build-host-setup.sh
+++ scripts/build-host-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # setup host for building
-sudo apt-get update
+sudo apt-get -qq update
 
 if [ -r ~/.aws/credentials ]; then
-    sudo apt-get install -y s3fs
+    sudo apt-get install -qqy s3fs
     mkdir -p ~/net/openamp-builds
     s3fs -o profile=openamp-builds-rw openamp-builds ~/net/openamp-builds
 fi
 
 # Yocto/OE requirements
-sudo apt-get install -y gawk wget git-core diffstat unzip texinfo \
+sudo apt-get install -qqy gawk wget git-core diffstat unzip texinfo \
   build-essential chrpath socat cpio python3 python3-pip \
   python3-pexpect xz-utils debianutils iputils-ping curl git \
   zstd libssl-dev lz4
